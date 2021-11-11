45fbf507b32797cfc09526d48935dd0871420a13
Tyrell Herbst <tyrellh589@gmail.com>
2021-11-11 18:00:56 +0200

    Register Page

---
+++ Project 2/Pages/Register.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_2.ViewModel;
+
+namespace Project_2.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public Register Model { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Project 2/ViewModel/Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_2.ViewModel
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Password and Confirmation Password did not match.") ]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
